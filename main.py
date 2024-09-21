810c8cabaaadc0e6eec5833f8f9f02bdfc8dc436
xxanny <68930876+francool57@users.noreply.github.com>
2024-09-21 13:46:39 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,297 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+import sql_operations
+from functools import wraps
+from werkzeug.security import check_password_hash
+from datetime import timedelta
+import os
+
+app = Flask(__name__)
+#app.secret_key = str(os.urandom(12).hex())  # Set a secret key for session management
+app.secret_key = 'dawdad'
+app.testing = True
+
+# Login required decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect('/')
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+
+    if sql_operations.get_user_by_email(email):
+        return jsonify({"success": False, "error": 'Email is already registered'})
+    
+    sql_operations.create_user(username, password, email)
+    user = sql_operations.get_user_by_email(email)
+    session['user_id'] = user[0]
+    return jsonify({"success": True, "redirect": url_for('index')})
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    user = sql_operations.get_user_by_email(email)
+
+    if user and check_password_hash(user[2], password):
+        session['user_id'] = user[0]
+        return jsonify({"success": True, "redirect": url_for('index')})
+    else:
+        return jsonify({"success": False, "error": 'Invalid credentials'})
+
+@app.route('/')
+def welcome():
+    if 'user_id' in session:
+        return redirect(url_for('index'))
+    return render_template('main.html')
+
+@app.route('/index')
+@login_required
+def index():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    tables = sql_operations.get_user_tables_preview(user_db)
+    if str(tables) == '[]':
+        return render_template('index.html', tables=tables, error='No tables found. Would you like to ')
+    else:   
+        return render_template('index.html', tables=tables)
+
+@app.route('/create_table', methods=['GET', 'POST'])
+@login_required
+def table_creator():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    
+    if request.method == 'POST':
+        table_name = request.form['table_name']
+        num_columns = int(request.form['num_columns'])
+        columns = []
+        for i in range(1, num_columns + 1):
+            column_name = request.form[f'column_name_{i}']
+            column_type = 'text'
+            columns.append((column_name, column_type))
+        try:
+            false_columns = ["id", "ids", "id_user", "id_user_chart", 'order']
+            error_message = []
+            for column in columns:
+                if column[0].lower() in false_columns:
+                    error_message = f"Column name cannot be '{column[0]}'."
+                    return jsonify({"success": False, "message": error_message})
+            sql_operations.create_table(user_db, table_name, columns)
+            # Insert data if provided
+            data_rows = []
+            row_index = 0
+            while True:
+                row_data = {}
+                for column in columns:
+                    key = f"data_row_{row_index}_data_{column[0]}"
+                    if key in request.form:
+                        row_data[column[0]] = request.form[key]
+                    else:
+                        break
+                if row_data:
+                    data_rows.append(row_data)
+                    row_index += 1
+                else:
+                    break
+
+            for row_data in data_rows:
+                sql_operations.insert_data(table_name, row_data, user_db)
+            
+            # Fetch the newly created table data
+            table_html = sql_operations.get_data2(user_db, table_name)
+            
+            return jsonify({
+                "success": True, 
+                "message": f"Table '{table_name}' created successfully in users database.",
+                "table_html": table_html
+            })
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)})
+    
+    return render_template('table_creation.html', user_db=user_db)
+
+
+@app.route('/delete_table', methods=['POST'])
+@login_required
+def delete_table():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    table = request.form['table']
+    try:
+        sql_operations.delete_table(table, user_db)
+        return jsonify({"success": True, "message": f"Table '{table}' has been deleted from database '{user_db}'"})
+    except Exception as e:
+        return jsonify({"success": False, "message": f"Error deleting table: {str(e)}"})
+
+@app.route('/get_tables')
+@login_required
+def get_tables():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    tables = sql_operations.get_database_tables(user_db)
+    return jsonify(tables)
+
+@app.route('/get_columns/<table>')
+@login_required
+def get_columns(table):
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    columns = sql_operations.get_table_columns(user_db, table)
+    return jsonify(columns)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('user_id', None)
+    return redirect('/')
+
+@app.route('/update_table', methods=['GET', 'POST'])
+@login_required
+def update_table():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    if request.method == 'POST':
+        data = request.json
+        
+        table = next(item['value'] for item in data if item['name'] == 'table')
+        
+        # Extract column names and data
+        columns = [item['name'].rstrip('[]') for item in data if item['name'] != 'table']
+        columns = list(dict.fromkeys(columns))  # Remove duplicates while preserving order
+
+        # Group data by columns
+        column_data = {col: [] for col in columns}
+        for item in data:
+            if item['name'].rstrip('[]') in columns:
+                column_data[item['name'].rstrip('[]')].extend(item['value'])
+        # Determine the number of rows
+        num_rows = len(next(iter(column_data.values())))
+
+        try:
+            formatted_rows = []
+            for i in range(num_rows):
+                row_data = {col: column_data[col][i] for col in columns}
+                formatted_row = " ".join([f"{col}:{row_data[col]}" for col in columns])
+                formatted_rows.append(formatted_row)
+                sql_operations.insert_data(table, row_data, user_db)
+            
+            formatted_output = "\n".join(formatted_rows)
+            
+            return jsonify({
+                "success": True, 
+                "message": f"Data inserted successfully into table '{table}' in database '{user_db}'",
+                "formatted_data": formatted_output
+            })
+        except Exception as e:
+            if "01000" in str(e):
+                column = str(e).split("'")[1].split("'")[0]
+                return jsonify({"success": False, "message": "Column: '" + column + "' should not have special characters: ()+-*/=<>$#@!~`%^&*?;:[]\\{}|"})
+            else:
+                print("Error:", str(e))  # Debug print
+                return jsonify({"success": False, "message": f"Error inserting data: {str(e)}"})
+    
+    return render_template('update_table.html', user_db=user_db)
+
+@app.route('/delete_chart', methods=['POST'])
+@login_required
+def delete_chart():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    chart_id = request.form['chart_id']
+    success = sql_operations.delete_chart(user_db, chart_id)
+    return jsonify({"success": success})
+
+@app.route('/custom_query', methods=['GET', 'POST'])
+@login_required
+def custom_query():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    if request.method == 'POST':
+        query = request.form['query']
+        try:
+            result = sql_operations.execute_custom_query(user_db, query)
+            return result
+        except Exception as e:
+            return str(e), 400
+    return render_template('custom_query.html', user_db=user_db)
+
+@app.route('/get_table_data', methods=['POST'])
+@login_required
+def get_table_data():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    table_name = request.form['table']
+    result = sql_operations.get_data(user_db, table_name)
+    
+    # Ensure result is a list of lists (or tuples)
+    if result and isinstance(result[0], (list, tuple)):
+        headers = result[0]
+        rows = result[1:]
+    else:
+        headers = []
+        rows = []
+    
+    return render_template('table_data.html', headers=headers, rows=rows, table_name=table_name)
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    user = sql_operations.get_user(session['user_id'])
+    if request.method == 'POST':
+        new_username = request.form['username']
+        if new_username != user[1]:
+            sql_operations.update_username(user[0], new_username)
+            return render_template('profile.html', user=user, success=True)
+        else:
+            return render_template('profile.html', user=user, success=False)
+    return render_template('profile.html', user=user)
+
+
+@app.route('/delete_account', methods=['POST'])
+@login_required
+def delete_account():
+    user_id = session['user_id']
+    sql_operations.delete_user(user_id)
+    session.pop('user_id', None)
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/custom_charts', methods=['GET', 'POST'])
+@login_required
+def custom_charts():
+    user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+    tables = sql_operations.get_database_tables(user_db)
+    
+    if request.method == 'POST':
+        data = request.json
+        # Save the chart configuration to the database
+        chart_id = sql_operations.save_chart(user_db, data)
+        return jsonify({"success": True, "chart_id": chart_id})
+    
+    # Fetch saved charts
+    saved_charts = sql_operations.get_saved_charts(user_db)
+    
+    return render_template('custom_charts.html', tables=tables, saved_charts=saved_charts)
+
+@app.route('/get_chart_data', methods=['POST'])
+@login_required
+def get_chart_data():
+    try:
+        user_db = str(sql_operations.get_user(session['user_id'])[3].split('@')[0]) + '_db'
+        data = request.json
+        chart_data = sql_operations.get_chart_data(user_db, data)
+        if "error" in chart_data:
+            print(chart_data["error"])
+            return jsonify({"error": chart_data["error"]}), 500
+        return jsonify(chart_data)
+    except Exception as e:
+        print(f"Error fetching chart data: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    sql_operations.ensure_database_structure()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
---
+++ sql_operations.py
@@ -0,0 +1,458 @@
+import pandas as pd
+import mysql.connector.pooling
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db_config = {
+    "host": "localhost",
+    "user": "root", #xxannydev
+    "password": "oMemf2812!",  #nFCZhA-hWLN5xUmxaKI-ow
+    "ssl_disabled": True
+}
+
+users_db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "oMemf2812!",
+    "database": "websitedata",
+    "ssl_disabled": True
+}
+
+db_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **db_config)
+users_db_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="users_pool", pool_size=5, **users_db_config)
+
+def get_db_connection():
+    return db_pool.get_connection()
+
+def get_users_db_connection():
+    return users_db_pool.get_connection()
+
+# server-user ops
+
+def get_user_databases(email):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("USE " + email.split("@")[0] + "_db")
+        return email.split("@")[0] + "_db"
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_user(id):
+    connection = get_users_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        if not user:
+            raise ValueError(f"No user found with id {id}")
+        return user
+    except mysql.connector.Error as err:
+        connection.reconnect(attempts=3, delay=5)
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        if not user:
+            raise ValueError(f"No user found with id {id}")
+        return user
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_user_by_email(email):
+    connection = get_users_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        return cursor.fetchone()
+    finally:
+        cursor.close()
+        connection.close()
+
+def create_user(username, password, email):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    users_connection = get_users_db_connection()
+    users_cursor = users_connection.cursor()
+    try:
+        print('Backend-debug: ' + username + ":" + password + "::" + email)
+        cursor.execute("CREATE DATABASE IF NOT EXISTS " + email.split("@")[0] + "_db")
+        connection.commit()
+        hashed_password = generate_password_hash(password)
+        users_cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (username, hashed_password, email))
+        users_connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+        users_cursor.close()
+        users_connection.close()
+
+def update_username(id, new_username):
+    connection = get_users_db_connection()
+    cursor = connection.cursor()
+    try:
+        print("UPDATE users SET username = %s WHERE id = %s", (new_username, id))
+        cursor.execute("UPDATE users SET username = %s WHERE id = %s", (new_username, id))
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+# Read operations
+
+def get_database_tables(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        cursor.execute("SHOW TABLES WHERE Tables_in_" + database + " NOT LIKE 'user_charts'")
+        return [table[0] for table in cursor.fetchall()]
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_table_columns(database, table):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        cursor.execute(f"DESCRIBE {table}")
+        return [col[0] for col in cursor.fetchall()]
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_data(db_name, table_name):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {db_name}")
+        cursor.execute(f"SELECT * FROM {table_name}")
+        
+        # Fetch column names
+        column_names = [description[0] for description in cursor.description]
+        # Fetch all rows
+        rows = cursor.fetchall()
+        
+        # Return column names as the first row, followed by data rows
+        return [[column for column in column_names]] + list(rows)
+    finally:
+        cursor.close()
+        connection.close()
+
+# Write operations
+
+def create_table(database, table, columns):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        column_definitions = ""
+        for col in columns:
+            column_definitions += f"`{col[0]}` VARCHAR(255), "
+
+        cursor.execute(f"CREATE TABLE {table} (id int PRIMARY KEY AUTO_INCREMENT, {column_definitions.strip(', ')})")
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+def delete_table(table, database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        cursor.execute(f"DROP TABLE {table}")
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+def insert_data(table, data, database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    columns = get_table_columns(database, table)
+    columns = [col for col in columns if col != 'id']  # Exclude 'id' column
+    
+    placeholders = ", ".join(["%s"] * len(columns))
+    values = [data[col] for col in columns]
+    
+    columns = [("`" + col + "`") for col in columns if col != 'id']  # Exclude 'id' column
+    columns_str = ", ".join(columns)
+
+    cursor.execute(f"USE {database}")
+    query = f"INSERT INTO {table} ({columns_str}) VALUES ({placeholders})"
+    print(query)
+
+    
+    cursor.execute(query, values)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def delete_data(table, data, database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        cursor.execute(f"DELETE FROM {table} WHERE id = %s", (data['id'],))
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+def execute_custom_query(database, query):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        if query.strip().upper().startswith('SELECT'):
+            cursor.execute(query)
+            result = cursor.fetchall()
+            columns = [i[0] for i in cursor.description]
+            df = pd.DataFrame(result, columns=columns)
+            return df.to_html(classes='mystyle', index=False, escape=True)
+        else:
+            cursor.execute(query)
+            connection.commit()
+            return f"Query executed successfully. Rows affected: {cursor.rowcount}"
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_data_range(database, table, start_row, end_row):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        query = f"SELECT * FROM {table} LIMIT {start_row - 1}, {end_row - start_row + 1}"
+        cursor.execute(query)
+        df = pd.DataFrame(cursor.fetchall(), columns=get_table_columns(database, table))
+        return df.to_html(classes='mystyle', index=False)
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_user_tables_preview(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        tables = get_database_tables2(database)
+        previews = []
+        for table_info in tables:
+            table_name = table_info['name']
+            columns = get_table_columns(database, table_name)
+            cursor.execute(f"USE {database}")
+            cursor.execute(f"SELECT * FROM {table_name} LIMIT 3")
+            rows = cursor.fetchall()
+            if table_name != "user_charts":
+                previews.append({
+                    'name': table_name,
+                    'columns': columns,
+                    'rows': rows,
+                    'row_count': table_info['row_count']
+                })
+        return previews
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_database_tables2(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        cursor.execute("SHOW TABLES")
+        tables = [table[0] for table in cursor.fetchall()]
+        table_info = []
+        for table in tables:
+            if table != "user_charts":  
+                cursor.execute(f"SELECT COUNT(*) FROM {table}")
+                row_count = cursor.fetchone()[0]
+                table_info.append({'name': table, 'row_count': row_count})
+        return table_info
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_data2(database, table):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        query = f"SELECT * FROM {table}"  # Limit to 10 rows for preview
+        cursor.execute(query)
+        columns = [col[0] for col in cursor.description]
+        rows = cursor.fetchall()
+        
+        html = "<table class='table'><thead><tr>"
+        for col in columns:
+            html += f"<th>{col}</th>"
+        html += "</tr></thead><tbody>"
+        
+        for row in rows:
+            html += "<tr>"
+            for cell in row:
+                html += f"<td>{cell}</td>"
+            html += "</tr>"
+        
+        html += "</tbody></table>"
+        
+        if not rows:
+            html += "<p>No data available. You can add data using the 'Update Table' feature.</p>"
+        
+        return html
+    finally:
+        cursor.close()
+        connection.close()
+
+def save_chart(database, chart_data):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        create_user_charts_table(database)  # Ensure the table exists
+        cursor.execute(f"USE {database}")
+        query = """
+        INSERT INTO user_charts (name, type, table_name, x_axis, y_axis)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+        values = (
+
+            chart_data['name'], chart_data['type'], chart_data['table'], 
+            chart_data['x_axis'], chart_data['y_axis'] 
+        )
+        cursor.execute(query, values)
+        connection.commit()
+        return cursor.lastrowid
+    finally:
+        cursor.close()
+        connection.close()
+
+def create_user_charts_table(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS user_charts (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            type VARCHAR(50) NOT NULL,
+            table_name VARCHAR(255) NOT NULL,
+            x_axis VARCHAR(255) NOT NULL,
+            y_axis VARCHAR(255) NOT NULL,
+            color VARCHAR(20),
+            border_color VARCHAR(20),
+            border_width INT,
+            fill BOOLEAN,
+            tension FLOAT
+        )
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_saved_charts(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        create_user_charts_table(database)  # Ensure the table exists
+        cursor.execute(f"USE {database}")
+        query = "SELECT * FROM user_charts"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return results
+    finally:
+        cursor.close()
+        connection.close()
+
+def get_chart_data(database, chart_data):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        query = f"SELECT {chart_data['x_axis']}, {chart_data['y_axis']} FROM {chart_data['table']}"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+        labels = []
+        values = []
+        for row in result:
+            if row[0] is not None:
+                labels.append(str(row[0]))
+            else:
+                labels.append('')
+            
+            if row[1] is not None:
+                try:
+                    values.append(float(row[1]))
+                except ValueError:
+                    values.append(0)  # or any default value you prefer
+            else:
+                values.append(0)  # or any default value you prefer
+        return {"labels": labels, "values": values}
+    except mysql.connector.Error as err:
+        return {"error": str(err)}
+    except Exception as e:
+        return {"error": str(e)}
+    finally:
+        cursor.close()
+        connection.close()
+
+def delete_chart(database, chart_id):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        query = "DELETE FROM user_charts WHERE id = %s"
+        cursor.execute(query, (chart_id,))
+        connection.commit()
+        return cursor.rowcount > 0
+    finally:
+        cursor.close()
+        connection.close()
+
+def create_user_charts_table(database):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(f"USE {database}")
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS user_charts (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            type VARCHAR(50) NOT NULL,
+            table_name VARCHAR(255) NOT NULL,
+            x_axis VARCHAR(255) NOT NULL,
+            y_axis VARCHAR(255) NOT NULL
+        )
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
+    finally:
+        cursor.close()
+        connection.close()
+
+def ensure_database_structure():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("CREATE DATABASE IF NOT EXISTS websitedata")
+        cursor.execute("USE websitedata")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE
+            )
+        """)
+        connection.commit()
+    except mysql.connector.Error as err:
+        print(f"Error ensuring database structure: {err}")
+        raise
+    finally:
+        cursor.close()
+        connection.close()
